96e8c9f662d84e02775c3973d927c940f8d7bc2c
misha-diakovych <m.diakovych13@gmail.com>
2021-01-26 13:14:01 +0200

    adding dist subtree

---
+++ dist/precache-manifest.fd187890c3fdc23b742097938f345e46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370e6b7ba9d31adbf7a4",
+    "url": "/chess/css/app.76862881.css"
+  },
+  {
+    "revision": "7ea4af7d8401d2b43ee841bfa2abe89d",
+    "url": "/chess/img/user.7ea4af7d.png"
+  },
+  {
+    "revision": "734cbe02129580619620869ceae28d2c",
+    "url": "/chess/index.html"
+  },
+  {
+    "revision": "370e6b7ba9d31adbf7a4",
+    "url": "/chess/js/app.1575bb48.js"
+  },
+  {
+    "revision": "50eb9b86a2a02996bf5c",
+    "url": "/chess/js/chunk-vendors.6046c961.js"
+  },
+  {
+    "revision": "e86ae379742631f201424690be9043f0",
+    "url": "/chess/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/chess/robots.txt"
+  }
+]);
